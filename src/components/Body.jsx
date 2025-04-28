ae07189fcc9d440e5c8bbf43efbeccf0bb46b862
ankitsaini001 <ankitsaini594@gmail.com>
2025-04-29 00:20:37 +0530

    useEffect Hook;

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,22 +8,26 @@ const Body = () => {
   // Initialize directly with resList (don't wrap it inside {})
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     //Optional Chaining
     //console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  }
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  if (listOfRestaurants.length === 0) {
-     return <Shimmer />
-  }
-  
+  // Conditional Rendering
+  // if (listOfRestaurants.length === 0) {
+  //    return <Shimmer />
+  // }
 
   const handleClick = () => {
     const filteredList = listOfRestaurants.filter(
@@ -33,8 +37,13 @@ const Body = () => {
     console.log(filteredList);
   };
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
+      <div className="search-container">
+        <input type="text" className="search-box" placeholder="search..."/>
+      </div>
       <div className="filter-search">
         <button className="filter-btn" onClick={handleClick}>
           Top Rated Restaurants
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
+import { useState } from "react";
 import { LOGO } from "../Utils/constants";
 
 // Header Section
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="app-logo">
-          <img
-            src={LOGO}
-            alt="App Logo"
-          />
-        </div>
-        <div className="header-menu">
-          <ul className="nav-items">
-            <li>Home</li>
-            <li>Offers</li>
-            <li>Help</li>
-            <li>Sign In</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  //useState Hook
+  const[btnNameReact, setBtnNameReact] = useState('Sign In');
+  return (
+    <div className="header">
+      <div className="app-logo">
+        <img src={LOGO} alt="App Logo" />
       </div>
-    );
-  };
+      <div className="header-menu">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>Offers</li>
+          <li>Help</li>
+          <li>Sign In</li>
+          <li>Cart</li>
+          <button className="signup-btn" onClick={()=>{
+           // let btnValue = "Sign Up";
+           btnNameReact === "Sign In" ? setBtnNameReact('Sign Up') : setBtnNameReact('Sign In');
+          }}>{btnNameReact}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
